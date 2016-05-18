e6e4d5235a2b28c39ea650e733b412ca87216dc0
Aidas <Aidas>
2016-05-18 21:13:04 +0300

    returns true or false

--- lib/valid.rb
+++ lib/valid.rb
@@ -1,6 +1,15 @@
 
 class Automorphic
-  def valid?
+  attr_accessor :number
+  def initialize(number)
+    @number = number
+  end
 
+  def valid?
+    valid = true
+    num_arr = number.to_s.split('').reverse
+    num_arr_square = (number ** 2).to_s.split('').reverse
+    num_arr.each_index { |x| valid = false if num_arr[x] != num_arr_square[x]}
+    valid
   end
 end
